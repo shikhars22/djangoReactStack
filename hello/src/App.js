40ff7ec3f041ef12b5c8ca7c22eae33e5da20aba
shikhars22 <shikhars22@gmail.com>
2022-12-04 05:20:55 +0530

    Lesson 2 done

--- hello/src/App.js
+++ hello/src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by SKG
         </a>
       </header>
     </div>
