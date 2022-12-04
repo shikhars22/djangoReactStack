4ff9ca03ed0936eff910437fa7bf4c356ba9546e
shikhars22 <shikhars22@gmail.com>
2022-12-04 06:07:25 +0530

    Lesson 3 done

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
+import Employee from './components/Employee';
+
 
 function App() {
+  console.log('we are about to list the employees');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React by SKG
-        </a>
-      </header>
+      {
+      console.log('inside the return')}
+      {showEmployees ? 
+      <>
+      <Employee />
+      <Employee />
+      <Employee />
+      </>
+    :
+    <p>You cannot see the employees</p>
+    }
     </div>
   );
 }
---
+++ hello/src/components/Employee.js
@@ -0,0 +1,5 @@
+function Employee () {
+    return <><h1>test</h1><h3>Here is an employee</h3></>
+}
+
+export default Employee;
