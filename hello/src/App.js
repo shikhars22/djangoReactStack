38b2db1b8b637a9c529d6d2be908f9b864442ef9
shikhars22 <shikhars22@gmail.com>
2022-12-04 09:25:51 +0530

    Lesson 8 done

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,10 +1,46 @@
 import './index.css';
 import Employee from './components/Employee';
-import {useState} from 'react'
+import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const showEmployees = true;
   const [role, setRole] = useState('dev');
+  const [employees, setemployees] = useState(
+    [
+      {
+        name: 'Shikhar',
+        role: 'Developer',
+        img: 'https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg',
+      },
+      {
+        name: 'Shireesh',
+        role: 'Manager',
+        img: 'https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg',
+      },
+      {
+        name: 'Rufo',
+        role: 'Pet',
+        img: 'https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg',
+      },
+      {
+        name: 'Archana',
+        role: 'Architect',
+        img: 'https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg',
+      },
+      {
+        name: 'Puneet',
+        role: 'Investor',
+        img: 'https://images.pexels.com/photos/2741701/pexels-photo-2741701.jpeg',
+      },
+      {
+        name: 'Abhishek',
+        role: 'Witness',
+        img: 'https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg',
+      },
+    ]
+  );
+  
   return (
     <div className="App">
       {showEmployees ? (
@@ -21,17 +57,17 @@ function App() {
           } 
         />
         <div className='flex flex-wrap'>
-          <Employee name="Shikhar" role="CEO" img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg' />
-          <Employee name="Shireesh" role={role} img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg' />
-          <Employee name="Rufo"  img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg'/>
-        
-          <Employee name="Shikhar" role="CEO" img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg' />
-          <Employee name="Shireesh" role={role} img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg' />
-          <Employee name="Rufo"  img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg'/>
-        
-          <Employee name="Shikhar" role="CEO" img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg' />
-          <Employee name="Shireesh" role={role} img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg' />
-          <Employee name="Rufo"  img='https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg'/>
+          {employees.map((employee) => {
+            console.log(uuidv4());
+            return(
+              <Employee 
+                key={uuidv4()}
+                name={employee.name} 
+                role={employee.role} 
+                img={employee.img} 
+              />
+            );
+          })}
         </div>
       </>
     ) : (
