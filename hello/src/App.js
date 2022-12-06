e16101450e0df62096d3c91389a37e70d70a8827
shikhars22 <shikhars22@gmail.com>
2022-12-06 14:26:11 +0530

    Lesson 20 done

--- hello/src/App.js
+++ hello/src/App.js
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import Employees from './pages/Employees';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Customers from './pages/Customers';
+import Dictionary from './components/Dictionary';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/employees' element={<Employees />} />
           <Route path='/customers' element={<Customers />} />
+          <Route path='/dictionary' element={<Dictionary />} />
         </Routes>
       </Header>
     </BrowserRouter>
--- hello/src/components/Header.js
+++ hello/src/components/Header.js
@@ -6,7 +6,7 @@ import {NavLink} from 'react-router-dom'
 const navigation = [
   { name: 'Employees', href: '/employees'},
   { name: 'Customers', href: '/customers' },
-  { name: 'Projects', href: '/other' },
+  { name: 'Dictionary', href: '/dictionary' },
   { name: 'Calendar', href: '/other2' },
 ]
 
