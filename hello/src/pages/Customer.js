96b2151c00ac7d818ffd05e07fc3f70d36eab8c2
shikhars22 <shikhars22@gmail.com>
2022-12-13 01:08:39 +0530

    Lesson 38 done

--- hello/src/pages/Customer.js
+++ hello/src/pages/Customer.js
@@ -6,11 +6,19 @@ import { apiCustomerUrl, baseUrl, homeCustomersUrl } from "../shared";
 export default function Customer() {
 
     const [customer, setCustomer] = useState();
+    const [tempCustomer, setTempCustomer] = useState();
     const [notFound, setNotFound] = useState(false);
+    const [changed, setChanged] = useState(false);
     const navigate = useNavigate();
     let { id } = useParams();
 
     const url = baseUrl + apiCustomerUrl + id;
+
+    useEffect(() => {
+        console.log('customer', customer)
+        console.log('temp customer', tempCustomer)
+        console.log('changed? ', changed)
+    });
 
     useEffect(() => {
         // console.log('Test SKg ' + id)
@@ -22,22 +30,54 @@ export default function Customer() {
                 }
                 return response.json();
             })
-            .then((data) => { setCustomer(data.customer); })
+            .then((data) => {
+                setCustomer(data.customer);
+                setTempCustomer(data.customer);
+            })
             .catch((e) => { console.log(e.message); })
     }, [])
+
+    function updateCustomer() {
+        console.log('inside update customer')
+        const url = baseUrl + apiCustomerUrl + id
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(tempCustomer)
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong')
+                }
+                return response.json();
+                // navigate(homeCustomersUrl)
+            })
+            .then((data) => {
+                setCustomer(data.customer);
+                setChanged(false)
+                console.log(data)
+            })
+            .catch((e) => { console.log(e) })
+    }
 
     function deleteCustomer() {
         // console.log('deelting.....');
         const url = baseUrl + apiCustomerUrl + id
-            fetch(url, {
-                method: 'DELETE', headers: {
-            'Content-Type':'application/json'
-        } }).then((response) => {
-            if (!response.ok) {
-                throw new Error('Something went wrong')
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type':'application/json'
             }
-            navigate(homeCustomersUrl)
-        }).catch((e) => {console.log(e)})
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong')
+                }
+                navigate(homeCustomersUrl)
+            })
+            .catch((e) => { console.log(e) })
     }
     return (
         <div>
@@ -55,15 +95,78 @@ export default function Customer() {
             }
             {customer
                 ? (
-                    <div>
-                        <p>{customer.id}</p>
-                        <p>{customer.name}</p>
-                        <p>{customer.industry}</p>
+                    <div className="flex justify-left">
+                        <ul>
+                            <input
+                                type={'text'}
+                                className="m-2 block shrink min-w-0 bg-gray-300 
+                                appearance-none border-2 border-gray-300 
+                                rounded w-full py-2 px-4 text-gray-700 
+                                leading-tight focus:outline-none 
+                                focus:bg-white focus:border-purple-500"
+                                value={tempCustomer.id} />
+                            <input
+                                type={'text'}
+                                className="m-2 block shrink min-w-0 bg-gray-300 
+                                appearance-none border-2 border-gray-300 
+                                rounded w-full py-2 px-4 text-gray-700 
+                                leading-tight focus:outline-none 
+                                focus:bg-white focus:border-purple-500"
+                                value={tempCustomer.name}
+                                onChange={(e) => {
+                                    setTempCustomer({
+                                        ...tempCustomer,
+                                        name: e.target.value,
+                                    });
+                                    console.log(e.target.value);
+                                    setChanged(true);
+                                }}
+                            />
+                            <input
+                                type={'text'}
+                                className="m-2 block shrink min-w-0 bg-gray-300 
+                                appearance-none border-2 border-gray-300 
+                                rounded w-full py-2 px-4 text-gray-700 
+                                leading-tight focus:outline-none 
+                                focus:bg-white focus:border-purple-500"
+                                value={tempCustomer.industry}
+                                onChange={(e) => {
+                                    setTempCustomer({
+                                        ...tempCustomer,
+                                        industry: e.target.value,
+                                    })
+                                    console.log(e.target.value);
+                                    setChanged(true);
+                                }}
+                            />
+                        </ul>
                     </div>
                 )
                 : null
             }
-            <br /><br />
+            
+            {changed
+                ? 
+                    <div className="flex">
+                        <br /><br />
+                        <button
+                            onClick={() => {
+                                    setTempCustomer({...customer})
+                                }
+                            }
+                        >
+                        Cancel
+                        </button>
+                        <button
+                            onClick={updateCustomer}
+                        >
+                            Save
+                        </button>
+                    </div>
+                :
+                    null
+            }
+            {/* <br /><br /> */}
             <button
                 className='bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded' 
                 onClick={(e) => {deleteCustomer()}}>
